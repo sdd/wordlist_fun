a4c750ffe164cfcf6388ffcd0d73fee8748cd800
Scott Donnelly <scott@donnel.ly>
2015-01-15 22:21:52 +0000

    optimised alternating algo: removed unneeded siblings sibling filter, unneeded uniq() call, unneeded test

--- algorithms/longest/alternatingMostLeastSiblings.js
+++ algorithms/longest/alternatingMostLeastSiblings.js
@@ -7,68 +7,40 @@
  */
 var _ = require('lodash-contrib');
 
-// best with this so far is amok, 1457
-
 var longest = function(chain, blackList, maxChain) {
 
 	blackList = blackList || [];
 	maxChain = maxChain || [];
 
-	process.stdout.write('Chain Length: ' + chain.length + ', Max Chain Length: ' + maxChain.length + ', Blacklist length ' + blackList.length + '        \r');
+	process.stdout.write('Chain Length: ' + chain.length + ', Max Chain Length: ' + maxChain.length + '     \r');
 
-	// precalculate a couple of commonly used things
 	var last = _.last(chain);
 	var chainWords = _.pluck(chain, 'word');
 
-	//
 	last.siblings = _.sortBy(
-		_.reject(last.siblings,
-			function(s) { return _.contains(chainWords, s.word) || _.contains(blackList, s.word); }
-		),
+		_.reject(last.siblings, function(s) { return _.contains(chainWords.concat(blackList), s.word); }),
 		function(s) { return -s.siblings.length; }
 	);
 
-	var backtrackCounter = 0;
-	while(last && last.siblings.length === 0) {
-
-		if (chainWords.length > maxChain.length) {
-			maxChain = _.clone(chainWords);
-		}
+	// backtrack if we have hit a dead end.
+	while(last && !last.siblings.length) {
+		if (chainWords.length > maxChain.length) { maxChain = _.clone(chainWords); }
 		_.times(2, function() { blackList.push(chain.pop().word); });
-		blackList = _.unique(blackList);
-
 		last = _.last(chain);
 
-		if (++backtrackCounter > 10) {
-			console.log('Backtrack limit reached');
-			blackList = _.difference(blackList, maxChain);
+		if (!last) {
+			blackList = _.difference(_.unique(blackList), maxChain);
 			return _.done({chain: maxChain, remainder: blackList});
 		}
 	}
 
-	if (!last) {
-		blackList = _.difference(blackList, maxChain);
-		return _.done({ chain: maxChain, remainder: blackList });
+	// alternate next word between sibling with most and sibling with least (but >1) siblings
+	var nextItemIndex = 0;
+	if (chain.length % 2 == 0) {
+		nextItemIndex = last.siblings.length - 1;
+		while (nextItemIndex > 0 && last.siblings[nextItemIndex].siblings.length <= 1) nextItemIndex--;
 	}
-
-	// pre-trim siblings' siblings
-	_.each(last.siblings, function(sibling, index, col) {
-		col[index].siblings = _.sortBy(
-			_.reject(
-				sibling.siblings,
-				function(s) { return _.contains(chainWords, s.word) || _.contains(blackList, s.word); }
-			),
-			function(s) { return -s.siblings.length; }
-		);
-	});
-
-	if (chain.length % 2 == 1) {
-		return function() { return longest(chain.concat(last.siblings[0]), blackList, maxChain); };
-	} else {
-		var i = last.siblings.length - 1;
-		while (i > 0 && last.siblings[i].siblings.length <= 1) i--;
-		return function() { return longest(chain.concat(last.siblings[i]), blackList, maxChain); };
-	}
+	return function() { return longest(chain.concat(last.siblings[nextItemIndex]), blackList, maxChain); };
 };
 
 module.exports = _.partial(_.trampoline, longest);
